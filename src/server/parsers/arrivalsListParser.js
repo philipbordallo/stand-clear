1509bce40fa2c50b4dd7009cef939da0007c871e
Philip Bordallo <git@philipbordallo.com>
2018-11-13 20:20:42 -0800

    Make arrivalID unique

--- src/server/parsers/arrivalsListParser.js
+++ src/server/parsers/arrivalsListParser.js
@@ -1,12 +1,20 @@
+import crypto from 'crypto';
+
 function arrivalsListParser(data) {
   return data.reduce((collection, location) => {
     const estimates = location.estimate.map((arrival, index) => {
+      const nameHash = crypto.createHash('md5')
+        .update(location.destination)
+        .digest('hex')
+        .slice(0, 7);
+
       const arrivalNumber = Number(index + 1)
         .toLocaleString('en-US', {
           minimumIntegerDigits: 2,
           useGrouping: false,
         });
-      const arrivalID = `${location.abbreviation}${arrivalNumber}`;
+
+      const arrivalID = `${location.abbreviation}${arrivalNumber}_${nameHash}`;
 
       return {
         arrivalID,
