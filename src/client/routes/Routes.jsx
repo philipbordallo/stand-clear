95c368f464166f8ef5556295cf13dfe9533857f4
Philip Bordallo <git@philipbordallo.com>
2018-12-09 23:46:26 -0800

    Add NotFoundPage for Oakland Airport

--- src/client/routes/Routes.jsx
+++ src/client/routes/Routes.jsx
@@ -8,14 +8,22 @@ import AdvisoriesPage from 'pages/AdvisoriesPage';
 import SettingsPage from 'pages/SettingsPage';
 import ErrorPage from 'pages/ErrorPage';
 
+
+function NotFoundPage() {
+  return (
+    <ErrorPage title="404" message="Page not found" />
+  );
+}
+
 function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={ StationsPage } />
+      <Route path="/station/oakl" component={ NotFoundPage } />
       <Route path="/station/:station" component={ DeparturesPage } />
       <Route path="/advisories" component={ AdvisoriesPage } />
       <Route path="/settings" component={ SettingsPage } />
-      <Route component={ () => <ErrorPage title="404" message="Page not found" /> } />
+      <Route component={ NotFoundPage } />
     </Switch>
   );
 }
